286167c651d7e9099c291da3f3245f04ff8e842b
YigitKral1 <69107835+YigitKral1@users.noreply.github.com>
2022-09-11 15:58:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,25 @@ sudo apt install nano -y
 mkdir McServer
 cd McServer
 clear
-read -p "Version: " versiyon
-wget https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/141/downloads/paper-$versiyon-141.jar
-mv paper-$versiyon-141.jar server.jar
+echo "Avaible Types:"
+echo "1) Paper"
+echo "2) Bukkit"
+echo "3) Spigot"
+read -p "Type: " type
+clear
+echo "Q: How to put your version?"
+echo "A: Please put it like '1.19.2' etc."
+read -p "Version: " version
+if [ type = "1" ]
+then
+wget https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/141/downloads/paper-$version-141.jar
+mv paper-$version-141.jar server.jar
+elif [ type = "2" ]
+then 
+wget https://download.getbukkit.org/craftbukkit/craftbukkit-$version.jar
+mv craftbukkit-$version.jar server.jar
+else
+wget https://download.getbukkit.org/spigot/spigot-$version.jar
+mv spigot-$version.jar server.jar
+fi
 rm ~/setup.sh
