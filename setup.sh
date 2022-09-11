29654854ba92e29e03036ab76eee5fcf42a2749e
YigitKral1 <69107835+YigitKral1@users.noreply.github.com>
2022-09-11 15:41:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ sudo apt install openjdk-17-jre-headless -y
 sudo apt install nano -y
 mkdir McServer
 cd McServer
-nano server.sh
-hello
+read -p "Version: " versiyon
+wget https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/141/downloads/paper-$versiyon-141.jar
