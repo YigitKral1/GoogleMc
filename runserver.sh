e4e3dbb1aba0cbc14dc96ae6d08528b74b892724
YigitKral1 <69107835+YigitKral1@users.noreply.github.com>
2022-09-11 20:56:53 +0300

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,5 @@ cd ~/McServer
 
 while true
 do
-mem=$(grep MemTotal /proc/meminfo | sed -e 's/MemTotal:[ ]*//' | sed -e 's/ kB//') # some new stuff 
-mem=$(($mem/1024/1024))
-java -Xmx$memG -Xms$memG -jar server.jar nogui 
+java -Xmx59G -Xms7G -jar server.jar nogui 
 done
