de6bebda1be904880f4bccc1bbe9d110fd8feb03
YigitKral1 <69107835+YigitKral1@users.noreply.github.com>
2022-09-11 16:44:16 +0300

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,5 @@
-while true
-do
 cd McServer
 screen -S Ngrok
 screen -X -S Ngrok ./ngrok tcp 25565
 screen -S Server
-screen -X -S Server java -Xmx1024M -Xms1024M -jar server.jar nogui
-done
+screen -X -S while true do Server java -Xmx1024M -Xms1024M -jar server.jar nogui done
